f40da7b5fe05b39543371bb3df6520a784c546d6
PetyaWebDesign <petyawebdesign@gmail.com>
2022-01-02 10:58:28 +0200

    modified the slider

--- js/main.js
+++ js/main.js
@@ -2,20 +2,14 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
+    effect: 'fade',
+    autoplay: true,
+
 
     // If we need pagination
     pagination: {
         el: '.swiper-pagination',
-    },
+    }
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
 
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
 });
